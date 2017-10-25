e12af5478c390dc6028cc7773d9b642e6e79e55a
Brian Sokolnicki <brian@sokolnicki.com>
2017-10-25 16:13:39 -0500

    TOC unique anchor tag fix

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,17 +1,23 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+
+$headers_history = {}
+
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
-    @head_count = {}
   end
+
   def header(text, header_level)
     friendly_text = text.parameterize
-    @head_count[friendly_text] ||= 0
-    @head_count[friendly_text] += 1
-    if @head_count[friendly_text] > 1
-      friendly_text += "-#{@head_count[friendly_text]}"
+    $headers_history[header_level] = text.parameterize
+
+    if header_level > 1
+      for i in (header_level - 1).downto(1)
+        friendly_text.prepend("#{$headers_history[i]}-") if $headers_history.key?(i)
+      end
     end
+
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
   end
 end
